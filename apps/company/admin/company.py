5f14909023ea7da35ea7e5395cf9ccb781f00f4d
xxx <dfg@sdf.ee>
2026-03-06 16:04:29 +0300

    fix

--- apps/company/admin/company.py
+++ apps/company/admin/company.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from apps.company.models.company import Company, CompanyType
 from apps.company.models.head import CompanyHead, Head
+from apps.company.models.founder import CompanyFounder, Founder
 
 @admin.register(CompanyType)
 class CompanyTypeAdmin(admin.ModelAdmin):
@@ -10,8 +11,18 @@ class CompanyTypeAdmin(admin.ModelAdmin):
 class HeadAdmin(admin.ModelAdmin):
     list_display = ['fio',]
 
+
+@admin.register(Founder)
+class FounderAdmin(admin.ModelAdmin):
+    list_display = ['fio_company', ]
+
+
 class CompanyHeadInline(admin.TabularInline):
     model = CompanyHead
+    extra = 0
+
+class CompanyFounderInline(admin.TabularInline):
+    model = CompanyFounder
     extra = 0
 
 @admin.register(Company)
@@ -20,4 +31,4 @@ class CompanyAdmin(admin.ModelAdmin):
     list_filter = ['company_type', 'founding_date']
     search_fields = ['name', 'inn', ]
     date_hierarchy = 'founding_date'
-    inlines = [CompanyHeadInline]
+    inlines = [CompanyHeadInline,CompanyFounderInline]
--- apps/company/models/founder.py
+++ apps/company/models/founder.py
@@ -3,12 +3,29 @@ from django.db import models
 from apps.common.models import BaseModel
 
 
+
 class Founder(BaseModel):
-    fio = models.CharField('ФИО', max_length=255, null=True, blank=False)
+    fio_company = models.CharField('ФИО\Название компании', max_length=255, null=True, blank=False)
+    inn = models.CharField('ИНН', max_length=255, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Учредитель компании'
         verbose_name_plural = 'Учредители компаний'
 
     def __str__(self):
-        return f"{self.fio}"
+        return f"{self.fio_company}"
+
+
+class CompanyFounder(BaseModel):
+    company = models.ForeignKey('Company', on_delete=models.CASCADE,related_name='founders')
+    founder = models.ForeignKey('Founder', on_delete=models.CASCADE)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    is_active = models.BooleanField(default=True, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Учредитель конкретной компании'
+        verbose_name_plural = 'Учредители конкретной компаний'
+
+    def __str__(self):
+        return f"Учредители {self.company} {self.founder}"
